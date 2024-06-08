92878d603e1524d9746da514925078e18f3dbf83
MostafaZayed72 <zayed8646@gmail.com>
2024-06-09 02:56:50 +0300

    feat : first commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,32 @@
 <template>
     <div>
-        <SectionsDemo1Section1 />
-        <SectionsDemo1Section2 />
-        <SectionsDemo1Section3 />
-        <SectionsDemo1Section4 />
-        <SectionsDemo1Section5 />
-        <SectionsDemo1Section6 />
-        <SectionsDemo1Section7 />
-        <SectionsDemo1Section8 />
-        <SectionsDemo1Section9 />
-        <SectionsDemo1Section10 />
-        <SectionsDemo1Section11 />
-        <SectionsDemo1Section12 />
-        <SectionsDemo1Section13 />
-        <SectionsDemo1Section14 />
-        <SectionsDemo1Section15 />
-        <SectionsDemo1Section16 />
-        <SectionsDemo1Section17 />
-        <SectionsDemo1Section18 />
-        <SectionsDemo1Section19 />
-        <SectionsDemo1Section20 />
-        <SectionsDemo1Section21 />
+        <SectionsDemo26Section1 />
+        <!-- <SectionsDemo26Section2 /> -->
+        <SectionsDemo26Section3 />
+        <SectionsDemo26Section4 />
+        <SectionsDemo26Section5 />
+        <!-- <SectionsDemo26Section6 />
+        <SectionsDemo26Section7 />
+        <SectionsDemo26Section8 />
+        <SectionsDemo26Section9 />
+        <SectionsDemo26Section10 />
+        <SectionsDemo26Section11 />
+        <SectionsDemo26Section12 />
+        <SectionsDemo26Section13 />
+        <SectionsDemo26Section14 />
+        <SectionsDemo26Section15 />
+        <SectionsDemo26Section16 />
+        <SectionsDemo26Section17 />
+        <SectionsDemo26Section18 />
+        <SectionsDemo26Section19 />
+        <SectionsDemo26Section20 /> -->
     </div>
 </template>
 
 <script setup>
     useHead({
         bodyAttrs: {
-            class: "navbar-light"
+            class: "navbar-light scheme-pink"
         },
     })
 </script>
